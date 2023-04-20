7e683d95516f5c04d30f5b7a4762d0f11e08bd8a
Segun Osiki <osiki.segun@gmail.com>
2023-04-20 15:13:14 +0100

    commit innitial file

---
+++ src/main/java/com/osiki/finteckafrika/FinteckAfrikaApplication.java
@@ -0,0 +1,13 @@
+package com.osiki.finteckafrika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinteckAfrikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinteckAfrikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osiki/finteckafrika/FinteckAfrikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiki.finteckafrika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinteckAfrikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
