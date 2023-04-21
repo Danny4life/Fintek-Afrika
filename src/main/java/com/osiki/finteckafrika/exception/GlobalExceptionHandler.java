94e8cca44122d52f00ab8b2ecf98bb6f7063e255
Segun Osiki <osiki.segun@gmail.com>
2023-04-22 00:59:42 +0100

    handle email not valid exception logic inside the global exception handler

--- src/main/java/com/osiki/finteckafrika/exception/GlobalExceptionHandler.java
+++ src/main/java/com/osiki/finteckafrika/exception/GlobalExceptionHandler.java
@@ -1,7 +1,24 @@
 package com.osiki.finteckafrika.exception;
 
+import com.osiki.finteckafrika.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Optional;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(EmailNotValidException.class)
+    @ResponseStatus(HttpStatus.ALREADY_REPORTED)
+    public ResponseEntity<ErrorResponse> handleEmailNotValidException(final EmailNotValidException exception){
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage(exception.getMessage());
+        errorResponse.setDebugMessage("Email not valid");
+
+        return ResponseEntity.of(Optional.of(errorResponse));
+
+    }
 }
