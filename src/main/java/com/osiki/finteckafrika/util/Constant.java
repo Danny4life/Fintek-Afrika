28a6e0a1ddbae85d00ae10b4bbf724b2b94e8ca8
Segun Osiki <osiki.segun@gmail.com>
2023-04-21 16:59:18 +0100

    create constant util class

---
+++ src/main/java/com/osiki/finteckafrika/util/Constant.java
@@ -0,0 +1,6 @@
+package com.osiki.finteckafrika.util;
+
+public class Constant {
+
+    public static final String EMAIL_VERIFICATION_TOKEN_LINK = "http://localhost:3333/api/v1/user/confirm?token=";
+}
