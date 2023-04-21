fc36659ccc36e78ae06f4a38749c6b126e20a2e6
Segun Osiki <osiki.segun@gmail.com>
2023-04-21 10:22:26 +0100

    edite

--- src/main/java/com/osiki/finteckafrika/service/serviceImpl/RegistrationServiceImpl.java
+++ src/main/java/com/osiki/finteckafrika/service/serviceImpl/RegistrationServiceImpl.java
@@ -5,13 +5,7 @@ import com.osiki.finteckafrika.repository.UsersRepository;
 import com.osiki.finteckafrika.service.RegistrationService;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import javax.validation.Valid;
-
 @Service
 @AllArgsConstructor
 public class RegistrationServiceImpl implements RegistrationService {
