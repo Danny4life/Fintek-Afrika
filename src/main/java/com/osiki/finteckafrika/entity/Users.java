cb47b4ccf701768f69ea43de70f2d26e397273ad
Segun Osiki <osiki.segun@gmail.com>
2023-04-21 09:33:32 +0100

    create users class entity

--- src/main/java/com/osiki/finteckafrika/entity/Users.java
+++ src/main/java/com/osiki/finteckafrika/entity/Users.java
@@ -1,12 +1,14 @@
 package com.osiki.finteckafrika.entity;
 
+import com.osiki.finteckafrika.enums.UsersStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -16,11 +18,32 @@ import javax.persistence.Table;
 @Table(name = "user_tbl")
 public class Users extends BaseClass {
 
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
+
+    @Email
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(length = 11, unique = true, nullable = false)
     private String bvn;
+
+    @Column(length = 11)
+    @Size(max = 11, min = 11, message = "Phone number must be 11 digits")
     private String phoneNumber;
+
+    @Column(nullable = false)
+    @Size(min = 8, message = "Password should not be less than 8 characters")
     private String password;
+
+    @Size(max = 6, min = 6, message = "Transaction pin must be 6 characters")
     private String transactionPin;
+
+    @Enumerated(EnumType.STRING)
+    private UsersStatus usersStatus = UsersStatus.INACTIVE;
+
+    private String role = "USER";
 }
