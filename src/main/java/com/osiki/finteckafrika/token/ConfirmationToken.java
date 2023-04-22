95495e92b0f51a4b8fcb333f111c737bccfd1974
Segun Osiki <osiki.segun@gmail.com>
2023-04-22 10:22:57 +0100

    create confirmation class entity

---
+++ src/main/java/com/osiki/finteckafrika/token/ConfirmationToken.java
@@ -0,0 +1,48 @@
+package com.osiki.finteckafrika.token;
+
+import com.osiki.finteckafrika.entity.Users;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class ConfirmationToken {
+
+    @Id
+    @SequenceGenerator(
+            name = "confirmation_token_sequence",
+            sequenceName = "confirmation_token_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "confirmation_token_sequence"
+    )
+
+
+    private Long id;
+    @Column(nullable = false)
+    private String token;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    @JoinColumn(nullable = false,
+                name = "user_id"
+    )
+    private Users users;
+
+    public ConfirmationToken(String token, LocalDateTime createdAt, LocalDateTime expiresAt, Users users) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.users = users;
+    }
+}
