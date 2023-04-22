2d2ee097626bfbb75e3daea56744d0fe3429f18a
Segun Osiki <osiki.segun@gmail.com>
2023-04-22 01:03:08 +0100

    inject email validator and mail service dependency

--- src/main/java/com/osiki/finteckafrika/service/serviceImpl/RegistrationServiceImpl.java
+++ src/main/java/com/osiki/finteckafrika/service/serviceImpl/RegistrationServiceImpl.java
@@ -3,6 +3,7 @@ package com.osiki.finteckafrika.service.serviceImpl;
 import com.osiki.finteckafrika.model.UserRegistrationRequestModel;
 import com.osiki.finteckafrika.repository.UsersRepository;
 import com.osiki.finteckafrika.service.RegistrationService;
+import com.osiki.finteckafrika.validations.EmailValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,8 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     private final UsersRepository usersRepository;
     private final UsersServiceImpl usersService;
+    private final EmailValidator emailValidator;
+    private final MailServiceImpl mailService;
 
 
     @Override
