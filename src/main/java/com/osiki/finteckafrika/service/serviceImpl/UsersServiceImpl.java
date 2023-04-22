7baa0e48d261a63e45ab9a5d2ac0d03814fd4f0b
Segun Osiki <osiki.segun@gmail.com>
2023-04-22 01:46:29 +0100

    create validate password method in Util class

--- src/main/java/com/osiki/finteckafrika/service/serviceImpl/UsersServiceImpl.java
+++ src/main/java/com/osiki/finteckafrika/service/serviceImpl/UsersServiceImpl.java
@@ -6,6 +6,9 @@ import com.osiki.finteckafrika.service.UsersService;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,12 +16,17 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 @Transactional
 @Builder
-public class UsersServiceImpl implements UsersService {
+public class UsersServiceImpl implements UserDetailsService, UsersService {
 
     private final UsersRepository usersRepository;
 
     @Override
     public String registerUser(UserRegistrationRequestModel registrationRequestModel) throws JSONException {
+        return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
     }
 }
