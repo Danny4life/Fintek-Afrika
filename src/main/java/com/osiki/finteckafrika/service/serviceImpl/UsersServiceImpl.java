fdfff89f7702424e8816377a79a7ceec13f3c3ed
Segun Osiki <osiki.segun@gmail.com>
2023-04-23 17:20:37 +0100

    inject password encoder, confirmation token repository and util dependency package

--- src/main/java/com/osiki/finteckafrika/service/serviceImpl/UsersServiceImpl.java
+++ src/main/java/com/osiki/finteckafrika/service/serviceImpl/UsersServiceImpl.java
@@ -2,8 +2,10 @@ package com.osiki.finteckafrika.service.serviceImpl;
 
 import com.osiki.finteckafrika.configuration.PasswordEncoder;
 import com.osiki.finteckafrika.model.UserRegistrationRequestModel;
+import com.osiki.finteckafrika.repository.ConfirmationTokenRepository;
 import com.osiki.finteckafrika.repository.UsersRepository;
 import com.osiki.finteckafrika.service.UsersService;
+import com.osiki.finteckafrika.util.Util;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import org.springframework.boot.configurationprocessor.json.JSONException;
@@ -21,6 +23,9 @@ public class UsersServiceImpl implements UserDetailsService, UsersService {
 
     private final UsersRepository usersRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+    private final ConfirmationTokenServiceImpl tokenService;
+    private final Util util;
 
     @Override
     public String registerUser(UserRegistrationRequestModel registrationRequestModel) throws JSONException {
