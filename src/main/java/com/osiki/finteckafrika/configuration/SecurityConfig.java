4b830e6e09360196e3fa05515ff1478f66f7e343
Segun Osiki <osiki.segun@gmail.com>
2023-04-24 14:15:09 +0100

    create security config class

--- src/main/java/com/osiki/finteckafrika/configuration/SecurityConfig.java
+++ src/main/java/com/osiki/finteckafrika/configuration/SecurityConfig.java
@@ -1,11 +1,21 @@
 package com.osiki.finteckafrika.configuration;
 
+import com.osiki.finteckafrika.util.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -15,4 +25,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final CustomAuthorizationFilter customAuthorizationFilter;
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http.cors()
+                .and()
+                .csrf()
+                .disable();
+        http.authorizeHttpRequests()
+                .antMatchers("/api/v1/user**").permitAll();
+        http.authorizeHttpRequests().antMatchers("api/v1/login/**", "api/v1/registration").permitAll();
+        http.authorizeHttpRequests().antMatchers(HttpMethod.GET, "/api/user/users/**")
+                .hasAnyAuthority("USER", "/api/v1/transactionHistory");
+
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.addFilterBefore(customAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
 }
