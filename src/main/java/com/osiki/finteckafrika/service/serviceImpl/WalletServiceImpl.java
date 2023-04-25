7c785114e47282d36218751eafa353fa31fa5d02
Segun Osiki <osiki.segun@gmail.com>
2023-04-25 12:06:08 +0100

    create wallet service class

---
+++ src/main/java/com/osiki/finteckafrika/service/serviceImpl/WalletServiceImpl.java
@@ -0,0 +1,15 @@
+package com.osiki.finteckafrika.service.serviceImpl;
+
+import com.osiki.finteckafrika.repository.UsersRepository;
+import com.osiki.finteckafrika.repository.WalletRepository;
+import com.osiki.finteckafrika.service.WalletService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WalletServiceImpl implements WalletService {
+
+    private final WalletRepository walletRepository;
+    private final UsersRepository usersRepository;
+}
