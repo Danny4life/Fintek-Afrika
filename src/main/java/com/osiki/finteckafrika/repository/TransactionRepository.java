3f9ea2ac068d2b0a7f8f62d17e328af416c4d437
Segun Osiki <osiki.segun@gmail.com>
2023-05-01 11:52:42 +0100

    create logic for all transactions method

--- src/main/java/com/osiki/finteckafrika/repository/TransactionRepository.java
+++ src/main/java/com/osiki/finteckafrika/repository/TransactionRepository.java
@@ -1,9 +1,17 @@
 package com.osiki.finteckafrika.repository;
 
 import com.osiki.finteckafrika.entity.Transaction;
+import com.osiki.finteckafrika.entity.Wallet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> finaAllByWallet(Wallet wallet);
+
+    Page<Transaction> findAllBySourceAccountNumberOrDestinationAccountNumber(String sender, String recipient, Pageable pageable);
 }
--- src/main/java/com/osiki/finteckafrika/service/serviceImpl/TransactionServiceImpl.java
+++ src/main/java/com/osiki/finteckafrika/service/serviceImpl/TransactionServiceImpl.java
@@ -1,5 +1,9 @@
 package com.osiki.finteckafrika.service.serviceImpl;
 
+import com.osiki.finteckafrika.entity.Transaction;
+import com.osiki.finteckafrika.entity.Users;
+import com.osiki.finteckafrika.entity.Wallet;
+import com.osiki.finteckafrika.exception.UserNotFoundException;
 import com.osiki.finteckafrika.model.TransactionHistoryModel;
 import com.osiki.finteckafrika.pagination_criteria.TransactionHistoryPages;
 import com.osiki.finteckafrika.repository.TransactionRepository;
@@ -7,11 +11,13 @@ import com.osiki.finteckafrika.repository.UsersRepository;
 import com.osiki.finteckafrika.repository.WalletRepository;
 import com.osiki.finteckafrika.service.TransactionService;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.data.domain.*;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +30,37 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public PageImpl<TransactionHistoryModel> allTransaction(TransactionHistoryPages transactionHistoryPages) {
-        return null;
+
+        String userEmail =  SecurityContextHolder.getContext().getAuthentication().getName();
+        Users users = usersRepository.findUsersByEmail(userEmail)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        Sort sort = Sort.by(transactionHistoryPages.getSortDirection(), transactionHistoryPages.getSortBy());
+        Pageable pageable = PageRequest.of(transactionHistoryPages.getPageNumber(), transactionHistoryPages.getPageSize(), sort);
+
+        Wallet wallet = walletRepository.findWalletByUsers(users);
+        List<Transaction> allTransactions = transactionRepository.finaAllByWallet(wallet);
+        String userAccountNumber = wallet.getAccountNumber();
+        Page<Transaction> transactions = transactionRepository
+                .findAllBySourceAccountNumberOrDestinationAccountNumber(userAccountNumber, userAccountNumber, pageable);
+
+        List<TransactionHistoryModel> response = new ArrayList<>();
+
+        for(Transaction transaction : allTransactions){
+            TransactionHistoryModel transactionHistoryModel = TransactionHistoryModel.builder()
+                    .id(transaction.getId())
+                    .name(transaction.getDestinationAccountName())
+                    .bank(transaction.getDestinationBank())
+                    .transactionTime(transaction.getCreatedAt())
+                    .transactionType(transaction.getTransactionType())
+                    .amount(transaction.getAmount())
+                    .build();
+
+            response.add(transactionHistoryModel);
+        }
+
+        PageImpl<TransactionHistoryModel> transactionHistoryPage = new PageImpl<>(response, pageable, transactions.getTotalElements());
+
+        return transactionHistoryPage;
     }
 }
